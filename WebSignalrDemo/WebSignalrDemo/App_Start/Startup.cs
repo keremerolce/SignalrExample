17f9300e33d20cb98abc4cdaf77facfa8ff4f498
Serhat Kerem EROL <keremerol.ce@outlook.com>
2019-02-27 04:29:32 +0300

    SignalR
    SignalR is C# programing explame

---
+++ WebSignalrDemo/WebSignalrDemo/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebSignalrDemo.Startup))]
+
+namespace WebSignalrDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ WebSignalrDemo/WebSignalrDemo/Hubs/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace WebSignalrDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string name,string message)
+        {
+            Clients.Others.GetMessageOther(name, message);
+
+            Clients.Caller.GetMessageCaller(message);
+        }
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public override Task OnConnected()
+        {
+            string id = Context.ConnectionId;
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
