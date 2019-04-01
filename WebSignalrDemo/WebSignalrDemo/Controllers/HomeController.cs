28b8549dcc33bad6c03776809cb85bc1141d7576
Serhat Kerem EROL <keremerol.ce@outlook.com>
2019-04-01 21:22:13 +0300

    Güncelleme

--- WebSignalrDemo/WebSignalrDemo/Controllers/HomeController.cs
+++ WebSignalrDemo/WebSignalrDemo/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace WebSignalrDemo.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
